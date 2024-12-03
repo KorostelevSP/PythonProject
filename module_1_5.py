1bbcef5600b3920c5cd26bf5e0e0a7c8d8330100
KorostelevSP <korostelevpython@gmail.com>
2024-12-03 12:50:49 +0300

    module_1_5 03/12/2024

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = ('моноблок',2,5,['строительный','черный'])
+print(immutable_var)
+
+# элементы кортежа не изменяются в явном виде
+# immutable_var[1] = 3
+
+mutable_list = ['полимерный материал',4,8]
+print(mutable_list)
+
+mutable_list[1] = 5
+print(mutable_list)
