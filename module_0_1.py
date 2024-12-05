bb269bb939db89174b6dce4bb27133e913bbaa56
KorostelevSP <korostelevpython@gmail.com>
2024-12-05 12:13:42 +0300

    module_0_1 05/12/2024

---
+++ module_0_1.py
@@ -0,0 +1,32 @@
+
+#"1st program"
+print(int(9**0.5*5))
+
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+
+#"3nd program", не то задание
+# a1 = 1234%1000//10
+# a2 = 5678%1000//10
+# print(a1+a2)
+
+#"3nd program"
+a = 2*2+2
+b = 2*(2+2)
+print(a==b)
+
+#"4th program", не то задание
+# a1 = 13.42
+# b1 = 42.13
+# a1_1 = int(a1)
+# a1_2 = round((a1-a1_1)*100)
+# b1_1 = int(b1)
+# b1_2 = round((b1-b1_1)*100)
+# print(a1_1==b1_2 or a1_2==b1_1)
+
+#"4th program"
+my_str = '123.456'
+my_number = float(my_str)
+my_number = int(my_number*10)
+my_number = my_number%10
+print(my_number)
