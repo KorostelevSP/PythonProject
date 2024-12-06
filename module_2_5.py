71656ab1fe2d3e9e99e37309a1c71fc04a4c9269
KorostelevSP <korostelevpython@gmail.com>
2024-12-06 16:52:34 +0300

    module_2_5 06/12/2024

--- module_2_5.py
+++ module_2_5.py
@@ -1,22 +1,20 @@
 # "Функции в Python.Функция с параметром"
 from module_2_3 import my_list
 
+
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        matrix.append([])  #
+        matrix.append([])
         for j in range(m):
-            matrix[i].append(value)  #
+            matrix[i].append(value)
     return matrix
 
+
 result1 = get_matrix(2, 2, 10)
-
 result2 = get_matrix(3, 5, 42)
-
 result3 = get_matrix(4, 2, 13)
 
 print(result1)
-
 print(result2)
-
 print(result3)
