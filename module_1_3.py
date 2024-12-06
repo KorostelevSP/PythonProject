0112ff341edc33ab1198013b2f5accf123ff1ec9
KorostelevSP <korostelevpython@gmail.com>
2024-12-06 16:52:21 +0300

    module_2_5 06/12/2024

--- module_1_3.py
+++ module_1_3.py
@@ -1,9 +1,10 @@
 from operator import truediv
+
 name = 'Sergei'
 print(name)
-age =  37
-print("Age: ",age)
-age = age+15
-print("New Age: ",age)
+age = 37
+print("Age: ", age)
+age = age + 15
+print("New Age: ", age)
 is_student = True
 print(is_student)
