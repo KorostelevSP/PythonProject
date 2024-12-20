d0744d71e9af0cf9c61095dbfe89633363eab0e6
KorostelevSP <korostelevpython@gmail.com>
2024-12-20 12:43:28 +0300

    module_3_6 20/12/2024

--- module_3_6.py
+++ module_3_6.py
@@ -16,8 +16,6 @@ def calculate_structure_sum(structure):
                     my_sum += value
                 elif isinstance(value, str):
                     my_sum += len(value)
-                # my_local_rez = calculate_structure_sum(value)
-                # my_sum += my_local_rez
         elif isinstance(i,int) or isinstance(i,float):
             my_sum += i
         elif isinstance(i, str):
@@ -26,3 +24,13 @@ def calculate_structure_sum(structure):
             print(type(i))
     return my_sum
 
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+global_rezult = calculate_structure_sum(data_structure)
+print(global_rezult)
