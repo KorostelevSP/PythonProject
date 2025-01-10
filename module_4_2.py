35fa75c64371e9c05a3922b692f64e0bbe4d01f1
KorostelevSP <korostelevpython@gmail.com>
2025-01-10 10:08:48 +0300

    module_4_2 10/01/2024

--- module_4_2.py
+++ module_4_2.py
@@ -1,8 +1,11 @@
 # "Пространство имен"
 def test_function():
+    print("Во внешней функции!")
     def inner_function():
-        print('Я в области видимости функции test_function')
-
-    inner_function()
+        return 'Я в области видимости функции test_function'
+    d = inner_function()
+    print(d)
 
 test_function()
+p = inner_function()
+print(p)
