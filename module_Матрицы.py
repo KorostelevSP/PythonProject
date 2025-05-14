700a74dba6dd0e3355a577a6bd36b70010eaa498
KorostelevSP <korostelevpython@gmail.com>
2025-05-14 17:44:27 +0300

    module_Матрицы 14/05/2025

--- module_Матрицы.py
+++ module_Матрицы.py
@@ -22,14 +22,30 @@ def main():
     #     matrix[i] = [0]*m
 
     # 3 способ
-    matrix = [[0] * m for i in range(n)]
-
+    matr_a = [[0] * m for i in range(n)]
+    matr_b = [[0] * n for i in range(m)]
     for i in range(n):
         for j in range(m):
             random_number = random.randint(1, 10)
-            matrix[i][j] = random_number
-            print(str(matrix[i][j]).rjust(2), end=' ')
+            matr_a[i][j] = random_number
+            print(str(matr_a[i][j]).rjust(2), end=' ')
         print()
+
+    print()
+
+    for i in range(m):
+        for j in range(n):
+            random_number = random.randint(1, 10)
+            matr_b[i][j] = random_number
+            print(str(matr_b[i][j]).rjust(2), end=' ')
+        print()
+
+    m_a = len(matr_a[0])
+    m_b = len(matr_b)
+    if m_a == m_b:
+        print('все норм')
+    else:
+        print('невозможно умножить')
 
 if __name__ == '__main__':
     main()
