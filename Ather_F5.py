aa9f672d2e5170d89dc197db929ea39e37eccb85
KorostelevSP <korostelevpython@gmail.com>
2025-06-17 16:57:47 +0300

    Ather_F5 15/06/2025

---
+++ Ather_F5.py
@@ -0,0 +1,32 @@
+def main():
+    colors = ['black', 'red', 'blue', 'green']
+    # for ind, item in enumerate(colors):
+    # for ind in range(colors):
+    #     print(ind,colors[ind])
+
+    for ind, item in enumerate(colors, 1):
+        print(ind, item)
+
+    # a = int(input())
+    # while a % 2 != 0:
+    #     a = int(input())
+    # print(a)
+
+    # i = 0
+    # a = int(input())
+    # while a != 0:
+    #     a = a // 10
+    #     i += 1
+    # print(i)
+
+    a = int(input())
+    flag = 'No'
+    while a != 0:
+        if a % 10 == 7:
+            flag = ('Yes')
+        a = a // 10
+    print(flag)
+
+
+if __name__ == '__main__':
+    main()
